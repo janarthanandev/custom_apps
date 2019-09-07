06a0cf35adae998f8adfd7387859a2e83d8e8959
EagleBlues <janarthanandevs@gmail.com>
2019-09-08 05:04:48 +0530

    worked on exchange

--- stock_exchange/models/stock_exchange.py
+++ stock_exchange/models/stock_exchange.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api
 from odoo.addons import decimal_precision as dp
+from mock.mock import self
 
 class stock_exchange(models.Model):
     _name = 'stock.exchange'
@@ -18,22 +19,22 @@ class stock_exchange(models.Model):
         ('confirm', 'Confirm'),
         ('done', 'Done'),
         ('cancel', 'Cancelled'),
-        ], string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', track_sequence=3, default='draft')
+        ], string='Status', copy=False,  track_visibility='onchange', track_sequence=3, default='draft')
     
-#     @api.onchange('order_line_ids','order_line.product_id')
-#     def _onchange_product_id(self):
-#         for line in self.order_line_ids:
-#             if line.product_id:
-#                 line.price_unit = line.product_id.lst_price
-#             else:
-#                 line.price_unit = 0.0
-#     @api.multi
-#     def write(self, vals):
-#         res = super(stock_exchange, self).write(vals)
-#         if 'order_line' in vals:
-#             vals['order_line'].mapped('order_id') = self.order_id
-#         return res
-    
+    @api.multi
+    def create_exchange(self):
+        print(self.exchange_line_ids)
+        order_id = self.order_id
+        order_line_obj = self.env['sale.order.line']
+        for exchange_line in self.exchange_line_ids:
+            order_line_obj.create({
+                'product_id': exchange_line.exchange_product_id.id,
+                'name': exchange_line.exchange_product_id.name,
+                'order_id': order_id.id,
+                'product_uom' : exchange_line.exchange_product_id.uom_id.id
+                                })
+            order_line_obj.product_id_change()
+        self.write({'state':'done'})
     @api.onchange('order_id')
     def onchange_order_id(self):
         self.order_line_ids = self.order_id.order_line
@@ -48,27 +49,11 @@ class Stock_Exchange_Line(models.Model):
     product_uom_qty = fields.Float(string='Ordered Quantity')
     product_uom = fields.Many2one('uom.uom', string='Unit of Measure')
     price_unit = fields.Float('Unit Price', required=True, digits=dp.get_precision('Product Price'), default=0.0)
-#     price_subtotal = fields.Monetary(compute='_compute_amount', string='Subtotal', readonly=True, store=True)
-#     price_tax = fields.Float(compute='_compute_amount', string='Total Tax', readonly=True, store=True)
-#     price_total = fields.Monetary(compute='_compute_amount', string='Total', readonly=True, store=True)
 #     
     @api.onchange('exchange_product_id')
     def _onchange_product_id(self):
         for line in self:
             if line.exchange_product_id:
-                line.product_uom = line.exchange_product_id.product_uom
                 line.price_unit = line.exchange_product_id.lst_price
             else:
                 line.price_unit = 0.0
-#     
-#     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
-#     def _compute_amount(self):
-#          for line in self:
-#             price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-#             taxes = line.tax_id.compute_all(price, line.exchange_id.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.exchange_id.order_id.partner_shipping_id)
-#             line.update({
-#                 'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-#                 'price_total': taxes['total_included'],
-#                 'price_subtotal': taxes['total_excluded'],
-#             })
-#     
